59f16acef8d0658b401e99a3ebd8f8e6b1344155
Matija Bulat <mbulat@racunarstvo.hr>
2024-07-25 02:05:00 +0200

    Add project files.

---
+++ ErrorModels/AppCustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace ignitech_backend.ErrorModels
+{
+    public class AppCustomException : Exception
+    {
+        public AppCustomException() : base() { }
+
+        public AppCustomException(string message) : base(message) { }
+
+        public AppCustomException(string message, params object[] args) : base(String.Format(message, args)) { }
+    }
+}
---
+++ Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using ignitech_backend.ErrorModels;
+using System.Net;
+
+namespace ignitech_backend.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+                var custom_message = "";
+
+                switch (error)
+                {
+                    case UnauthorizedAccessException:
+                        response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        custom_message = error.Message;
+                        break;
+                    case NotImplementedException:
+                        response.StatusCode = (int)HttpStatusCode.NotImplemented;
+                        custom_message = error.Message;
+                        break;
+                    case FluentValidationException:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        custom_message = error.Message;
+                        break;
+                    case SystemException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        custom_message = e.Message;
+                        break;
+                    case AppCustomException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        custom_message = e.Message;
+                        break;
+
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        custom_message = "An error occurred while processing your request.";
+                        break;
+                }
+                await response.WriteAsync(new ErrorDetails()
+                {
+                    StatusCode = response.StatusCode,
+                    Message = custom_message
+                }.ToString());
+            }
+        }
+    }
+}
